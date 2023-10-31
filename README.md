1c190a0e32dd408af5f7991a56c3e83d40385e26
antho czer <czer.anthony@gmail.com>
2023-10-31 11:20:00 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CustomFrameworkWFI
+
+A description of this package.
---
+++ Sources/CustomFrameworkWFI/CustomFrameworkWFI.swift
@@ -0,0 +1,6 @@
+public struct CustomFrameworkWFI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
