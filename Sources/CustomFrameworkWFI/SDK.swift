827d6b15a7db9762734dfcacbd43f905fa11217e
antho czer <czer.anthony@gmail.com>
2023-10-31 11:26:38 +0100

    Initial commit

---
+++ Sources/CustomFrameworkWFI/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Anto Czer on 31/10/2023.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork(){
+        print("Doing some work")
+    }
+}
